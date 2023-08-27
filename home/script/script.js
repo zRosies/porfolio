4754056988a81633ee5be1245e7420fc2481a56a
zRosies <gustavo.enrollment@gmail.com>
2023-08-26 21:56:16 -0300

    changes

--- home/script/script.js
+++ home/script/script.js
@@ -7,31 +7,9 @@ const bb = document.querySelector('.container1');
 
 const text = document.querySelector('.future');
 //Animation
-const textLoad = () => {
-  let count=0;
-  setTimeout(() => {
-    text.textContent = "Gustavo Bispo";
-  }, 0);
-  setTimeout(() => {
-    text.textContent = "Software Developer";
-  }, 3000);
-  setTimeout(() => {
-    text.textContent = "Web Developer";
-  }, 6000);
-  setTimeout(() => {
-    text.textContent = "The Future";
-  }, 9000);
-
-  setTimeout(() => {
-    textLoad(count + 1);
-    if(count>4){
-      count=0;
-    }
-  }, 12000);
-}
 
 
-textLoad();
+
     
 //Event Listeners
 registerlink.addEventListener('click',()=> {
@@ -72,7 +50,7 @@ function toggleMenu(){
       hamButton.innerHTML = "&#9776";
       }
   // document.querySelector(".navigation").style.display="block";
-  console.log("AAA")
+  
   
 }
 
@@ -94,3 +72,98 @@ const go = last.toLocaleString("pt-Br");
 year.textContent=yeardate;
 lastUpd.textContent=go;
 
+
+// Translating to portuguese//
+
+const switchButton = document.querySelector('#switchButton'); 
+let currentLanguage = 'english'; // 
+
+// Function to change language
+
+const textLoad = () => {
+  let count=0;
+  setTimeout(() => {
+    text.textContent = "Gustavo Bispo";
+  }, 0);
+  setTimeout(() => {
+    text.textContent = "Software Developer";
+  }, 3000);
+  setTimeout(() => {
+    text.textContent = "Web Developer";
+  }, 6000);
+  setTimeout(() => {
+    text.textContent = "The Future";
+  }, 9000);
+
+  setTimeout(() => {
+    textLoad(count + 1);
+    if(count>4){
+      count=0;
+    }
+  }, 12000);
+}
+
+const textLoadpT = () => {
+  let count=0;
+  setTimeout(() => {
+    text.textContent = "Gustavo Bispo";
+  }, 0);
+  setTimeout(() => {
+    text.textContent = "Desenvolvedor de Software";
+  }, 3100);
+  setTimeout(() => {
+    text.textContent = "Desenvolvedor Web";
+  }, 6200);
+  setTimeout(() => {
+    text.textContent = "O Futuro";
+  }, 9300);
+
+  setTimeout(() => {
+    textLoad(count + 1);
+    if(count>4){
+      count=0;
+    }
+  }, 12000);
+}
+
+let defaultLoad=true;
+
+
+function changeLanguage(language) {
+    fetch(`./script/translate.json`)
+        .then(response => response.json())
+        .then(translation => {
+            const elementsToTranslate = document.querySelectorAll('[data-translation]');
+            
+            elementsToTranslate.forEach(element => {
+                const translationKey = element.getAttribute('data-translation');
+                element.textContent = translation[language][translationKey]; 
+            });
+           
+        })
+        .catch(error => console.error('Error loading translation:', error));
+}
+
+switchButton.addEventListener('click', () => {
+
+    if (switchButton.checked) {
+        changeLanguage('portuguese');
+        currentLanguage = 'portuguese';
+        textLoadpT();
+        defaultLoad=false;
+        
+    }
+    else if(defaultLoad){
+
+      textLoad();
+
+    } else {
+        changeLanguage('english');
+        currentLanguage = 'english';
+        textLoad();
+        defaultLoad=false;
+    }
+});
+
+// Initial language setup
+changeLanguage(currentLanguage);
