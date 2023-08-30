2b51ae1da3e3a8a90a228c3f1e0a45c019a58498
zRosies <gustavo.enrollment@gmail.com>
2023-08-30 13:46:09 -0300

    changes

--- contact/script/script.js
+++ contact/script/script.js
@@ -38,3 +38,61 @@ year.textContent=yeardate;
 lastUpd.textContent=go;
 
 console.log(year);
+
+
+
+// Function to change language
+
+function changeLanguage(language) {
+  fetch(`./script/translate.json`)
+      .then(response => response.json())
+      .then(translation => {
+          const elementsToTranslate = document.querySelectorAll('[data-translation]');
+          
+          elementsToTranslate.forEach(element => {
+              const translationKey = element.getAttribute('data-translation');
+              element.textContent = translation[language][translationKey]; 
+          });
+         
+      })
+      .catch(error => console.error('Error loading translation:', error));
+}
+
+
+const switchButton = document.querySelector('#lang'); 
+const switchButton2 = document.querySelector('#lang2'); 
+let currentLanguage = 'english'; // 
+
+
+switchButton.addEventListener('change', () => {
+  console.log("aaaaaa");
+  const language = switchButton.value.toLowerCase();
+  if (language==="portuguese") {
+      changeLanguage('portuguese');
+      
+      
+  }
+  else {
+      changeLanguage('english');
+    
+      
+  }
+  // languageLoad(currentLanguage)
+});
+
+switchButton2.addEventListener('change', () => {
+console.log("aaaaaa");
+const language = switchButton2.value.toLowerCase();
+if (language==="portuguese") {
+    changeLanguage('portuguese');
+    
+    
+}
+else {
+    changeLanguage('english');
+  
+    
+}
+
+});
+
